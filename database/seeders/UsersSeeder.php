f8f4fecddf8180a9aeadc595889c718ade6dc5ef
Flavio Ariano <fhariano@gmail.com>
2022-10-26 14:27:44 -0300

    Ajustes em seed

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -29,5 +29,26 @@ class UsersSeeder extends Seeder
             'name' => 'Murilo Araújo',
             'password'  => Hash::make('mca@2022'),
         ]);
+        $user = User::create([
+            'bar_id' => 1,
+            'group_id' => 3,
+            'email' => 'mcablack3@gmail.com',
+            'name' => 'Murilo Araújo 3',
+            'password'  => Hash::make('mca@2022'),
+        ]);
+        $user = User::create([
+            'bar_id' => 1,
+            'group_id' => 4,
+            'email' => 'mcablack4@gmail.com',
+            'name' => 'Murilo Araújo 4',
+            'password'  => Hash::make('mca@2022'),
+        ]);
+        $user = User::create([
+            'bar_id' => 1,
+            'group_id' => 5,
+            'email' => 'mcablack5@gmail.com',
+            'name' => 'Murilo Araújo 5',
+            'password'  => Hash::make('mca@2022'),
+        ]);
     }
 }
