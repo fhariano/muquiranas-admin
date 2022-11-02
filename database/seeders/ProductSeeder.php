882b6c85fa73e25d0527343b79d5c6e7128727dc
mcablack <muriloctai10@gmail.com>
2022-11-02 01:22:04 -0300

    Atualização seeds

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -28,7 +28,7 @@ class ProductSeeder extends Seeder
             "price_base" => 12.00,
             "image_url" => "https://s3-sa-east-1.amazonaws.com/cake-app-files/public/customers/11083/products/7583007/thumb/d7e95e59-3e56-49d6-8f4c-809a597499f6.png",
             "order" => 1,
-            "inserted_for" => "Flavio Ariano",
+            "inserted_for" => "Murilo Araújo",
         ]);
         $products->create([
             "bar_id" => 1,
@@ -45,13 +45,13 @@ class ProductSeeder extends Seeder
             "price_base" => 8.00,
             "image_url" => "https://ws.ri2b.com.br/flutter-imgs/brahma.png",
             "order" => 2,
-            "inserted_for" => "Flavio Ariano",
+            "inserted_for" => "Murilo Araújo",
         ]);
         $products->create([
             "bar_id" => 1,
             "category_id" => 2,
             "erp_id" => 7611928,
-            "ean_erp" => "7501035010109",
+            "ean_erp" => "7896010002256",
             "name" => "TEQUILA JOSE CUERVO ESP REP",
             "short_name" => "JOSE CUERVO",
             "short_description" => "DOSE",
@@ -62,11 +62,11 @@ class ProductSeeder extends Seeder
             "price_base" => 18.00,
             "image_url" => "https://ws.ri2b.com.br/flutter-imgs/tequila-shot.png",
             "order" => 1,
-            "inserted_for" => "Flavio Ariano",
+            "inserted_for" => "Murilo Araújo",
         ]);
         $products->create([
             "bar_id" => 2,
-            "category_id" => 1,
+            "category_id" => 7,
             "erp_id" => 7583007,
             "ean_erp" => "7891149101900",
             "name" => "STELLA ARTOIS LONG NECK 330ML",
@@ -78,11 +78,11 @@ class ProductSeeder extends Seeder
             "price_base" => 12.00,
             "image_url" => "https://s3-sa-east-1.amazonaws.com/cake-app-files/public/customers/11083/products/7583007/thumb/d7e95e59-3e56-49d6-8f4c-809a597499f6.png",
             "order" => 2,
-            "inserted_for" => "Flavio Ariano",
+            "inserted_for" => "Murilo Araújo",
         ]);
         $products->create([
             "bar_id" => 2,
-            "category_id" => 1,
+            "category_id" => 7,
             "erp_id" => 7541193,
             "ean_erp" => "7891149030309",
             "name" => "CERVEJA BRAHMA CHOPP LONG NECK 355ML",
@@ -95,13 +95,13 @@ class ProductSeeder extends Seeder
             "price_base" => 8.00,
             "image_url" => "https://ws.ri2b.com.br/flutter-imgs/brahma.png",
             "order" => 1,
-            "inserted_for" => "Flavio Ariano",
+            "inserted_for" => "Murilo Araújo",
         ]);
         $products->create([
             "bar_id" => 2,
             "category_id" => 2,
             "erp_id" => 7611928,
-            "ean_erp" => "7501035010109",
+            "ean_erp" => "7896010002256",
             "name" => "TEQUILA JOSE CUERVO ESP REP",
             "short_name" => "JOSE CUERVO",
             "short_description" => "DOSE",
@@ -112,7 +112,7 @@ class ProductSeeder extends Seeder
             "price_base" => 18.00,
             "image_url" => "https://ws.ri2b.com.br/flutter-imgs/tequila-shot.png",
             "order" => 1,
-            "inserted_for" => "Flavio Ariano",
+            "inserted_for" => "Murilo Araújo",
         ]);
         $products->create([
             "bar_id" => 1,
@@ -129,7 +129,7 @@ class ProductSeeder extends Seeder
             "price_base" => 8.00,
             "image_url" => "https://ws.ri2b.com.br/flutter-imgs/brahmaextra-weiss.png",
             "order" => 3,
-            "inserted_for" => "Flavio Ariano",
+            "inserted_for" => "Murilo Araújo",
         ]);
         $products->create([
             "bar_id" => 1,
@@ -146,7 +146,7 @@ class ProductSeeder extends Seeder
             "price_base" => 4.00,
             "image_url" => "https://ws.ri2b.com.br/flutter-imgs/skol.png",
             "order" => 4,
-            "inserted_for" => "Flavio Ariano",
+            "inserted_for" => "Murilo Araújo",
         ]);
         $products->create([
             "bar_id" => 1,
@@ -163,7 +163,7 @@ class ProductSeeder extends Seeder
             "price_base" => 9.00,
             "image_url" => "https://ws.ri2b.com.br/flutter-imgs/becks.png",
             "order" => 5,
-            "inserted_for" => "Flavio Ariano",
+            "inserted_for" => "Murilo Araújo",
         ]);
         $products->create([
             "bar_id" => 1,
@@ -180,7 +180,7 @@ class ProductSeeder extends Seeder
             "price_base" => 12.00,
             "image_url" => "https://ws.ri2b.com.br/flutter-imgs/caipirinha.png",
             "order" => 2,
-            "inserted_for" => "Flavio Ariano",
+            "inserted_for" => "Murilo Araújo",
         ]);
         $products->create([
             "bar_id" => 1,
@@ -197,13 +197,13 @@ class ProductSeeder extends Seeder
             "price_base" => 3.00,
             "image_url" => "https://ws.ri2b.com.br/flutter-imgs/agua-sem-gas.png",
             "order" => 1,
-            "inserted_for" => "Flavio Ariano",
+            "inserted_for" => "Murilo Araújo",
         ]);
         $products->create([
             "bar_id" => 1,
             "category_id" => 4,
-            "erp_id" => 7611928,
-            "ean_erp" => "7501035010109",
+            "erp_id" => 7690531,
+            "ean_erp" => "9010330005966",
             "name" => "WHISKY JACK DANIEL'S",
             "short_name" => "WHISKY JACK DANIEL'S",
             "short_description" => "12 anos",
@@ -214,7 +214,7 @@ class ProductSeeder extends Seeder
             "price_base" => 150.00,
             "image_url" => "https://ws.ri2b.com.br/flutter-imgs/jack-daniels-garrafa.png",
             "order" => 1,
-            "inserted_for" => "Flavio Ariano",
+            "inserted_for" => "Murilo Araújo",
         ]);
         $products->create([
             "bar_id" => 1,
@@ -231,7 +231,7 @@ class ProductSeeder extends Seeder
             "price_base" => 15.90,
             "image_url" => "https://ws.ri2b.com.br/flutter-imgs/mini-pasteis.png",
             "order" => 1,
-            "inserted_for" => "Flavio Ariano",
+            "inserted_for" => "Murilo Araújo",
         ]);
         $products->create([
             "bar_id" => 1,
@@ -248,13 +248,13 @@ class ProductSeeder extends Seeder
             "price_base" => 28.90,
             "image_url" => "https://ws.ri2b.com.br/flutter-imgs/corona-pack.png",
             "order" => 1,
-            "inserted_for" => "Flavio Ariano",
+            "inserted_for" => "Murilo Araújo",
         ]);
         $products->create([
             "bar_id" => 2,
             "category_id" => 7,
-            "erp_id" => 7611928,
-            "ean_erp" => "7501035010109",
+            "erp_id" => 7689807,
+            "ean_erp" => "7891149106356",
             "name" => "BRAHMA EXTRA WEISS",
             "short_name" => "BRAHMA EXTRA WEISS",
             "short_description" => "LONG NECK",
@@ -265,7 +265,7 @@ class ProductSeeder extends Seeder
             "price_base" => 8.00,
             "image_url" => "https://ws.ri2b.com.br/flutter-imgs/brahmaextra-weiss.png",
             "order" => 3,
-            "inserted_for" => "Flavio Ariano",
+            "inserted_for" => "Murilo Araújo",
         ]);
         $products->create([
             "bar_id" => 2,
@@ -282,13 +282,13 @@ class ProductSeeder extends Seeder
             "price_base" => 4.00,
             "image_url" => "https://ws.ri2b.com.br/flutter-imgs/skol.png",
             "order" => 4,
-            "inserted_for" => "Flavio Ariano",
+            "inserted_for" => "Murilo Araújo",
         ]);
         $products->create([
             "bar_id" => 2,
             "category_id" => 7,
-            "erp_id" => 7611928,
-            "ean_erp" => "7501035010109",
+            "erp_id" => 7689812,
+            "ean_erp" => "4002631022501",
             "name" => "CERVEJA BECKS",
             "short_name" => "BECKS",
             "short_description" => "LONG NECK",
@@ -299,13 +299,13 @@ class ProductSeeder extends Seeder
             "price_base" => 9.00,
             "image_url" => "https://ws.ri2b.com.br/flutter-imgs/becks.png",
             "order" => 5,
-            "inserted_for" => "Flavio Ariano",
+            "inserted_for" => "Murilo Araújo",
         ]);
         $products->create([
             "bar_id" => 2,
             "category_id" => 8,
-            "erp_id" => 7611928,
-            "ean_erp" => "7501035010109",
+            "erp_id" => 7690294,
+            "ean_erp" => "7896050200452",
             "name" => "CAIPIRINHA",
             "short_name" => "CAIPIRINHA",
             "short_description" => "Pinga, Limão e Açúcar",
@@ -316,13 +316,13 @@ class ProductSeeder extends Seeder
             "price_base" => 12.00,
             "image_url" => "https://ws.ri2b.com.br/flutter-imgs/caipirinha.png",
             "order" => 2,
-            "inserted_for" => "Flavio Ariano",
+            "inserted_for" => "Murilo Araújo",
         ]);
         $products->create([
             "bar_id" => 2,
             "category_id" => 12,
-            "erp_id" => 7611928,
-            "ean_erp" => "7501035010109",
+            "erp_id" => 7690563,
+            "ean_erp" => "7501064194160",
             "name" => "CERVEJA CORONA PACK 6 UN",
             "short_name" => "CERVEJA CORONA",
             "short_description" => "PACK C/ 6",
@@ -333,7 +333,7 @@ class ProductSeeder extends Seeder
             "price_base" => 28.90,
             "image_url" => "https://ws.ri2b.com.br/flutter-imgs/corona-pack.png",
             "order" => 1,
-            "inserted_for" => "Flavio Ariano",
+            "inserted_for" => "Murilo Araújo",
         ]);
     }
 }
