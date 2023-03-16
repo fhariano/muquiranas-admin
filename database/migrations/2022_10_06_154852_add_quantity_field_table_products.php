0b9f3dbff41d7a7ec1e3297b92e5927873447628
Flavio Ariano <fhariano@gmail.com>
2023-03-16 17:49:12 -0300

    Ajustes

--- database/migrations/2022_10_06_154852_add_quantity_field_table_products.php
+++ database/migrations/2022_10_06_154852_add_quantity_field_table_products.php
@@ -14,11 +14,7 @@ class AddQuantityFieldTableProducts extends Migration
     public function up()
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->string('quantity',10)
-            ->nullable()
-            ->after('marca')
-            ->default(0);
-               
+            $table->decimal('price_cost_erp', $precision = 8, $scale = 2)->default(0.00)->after('marca');
         });
     }
 
