f7b217e437ee6b5290afba99e3b965c0b015281c
Flavio Ariano <fhariano@gmail.com>
2023-03-16 20:03:09 -0300

    Ajustes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,18 +16,18 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call([
             BarSeeder::class,
-            CategorySeeder::class,
-            ProductSeeder::class,
-            PromosListsSeeder::class,
-            ProductsPromosListsSeeder::class,
+            // CategorySeeder::class,
+            // ProductSeeder::class,
+            // PromosListsSeeder::class,
+            // ProductsPromosListsSeeder::class,
             ResourcesSeeder::class,
             GroupsSeeder::class,
             UsersSeeder::class,
             UsersBarsSeeder::class,
             PermissionsGroupsSeeder::class,
             OrderTypesSeeder::class,
-            OrdersSeeder::class,
-            OrdersItemsSeeder::class,
+            // OrdersSeeder::class,
+            // OrdersItemsSeeder::class,
         ]);
     }
 }
